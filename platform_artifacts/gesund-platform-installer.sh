3b4b870ad20bf830fbb5e4f6a89c85d16c6d58c3
Pedro Martins <pedromartins.cwb@gmail.com>
2023-08-21 15:23:07 -0300

    update installation instructions

---
+++ platform_artifacts/gesund-platform-installer.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+
+START_SERVICES=1
+
+show_help() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Install Gesund Platform"
+    echo "Options:"
+    echo "  -t, --tarball NAME     Specify the name of the encrypted tarball on S3 (required)"
+    echo "  -p, --decryption-password PASSWORD   Specify the decryption password (required)"
+    echo "  -h, --help             Show this help message and exit"
+}
+
+handle_error() {
+    echo "Error: $1" >&2
+    exit 1
+}
+
+# Function to show a spinner while a command is running
+spinner() {
+    local pid=$1
+    local delay=0.25
+    local spinstr="/|\\-"
+    while ps -p $pid > /dev/null; do
+        local temp=${spinstr#?}
+        printf " [%c] " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+wait_for_container() {
+    local container_name="$1"
+    local max_attempts=60  
+    local wait_seconds=2
+
+    for ((i = 0; i < max_attempts; i++)); do
+        if docker ps --format "{{.Names}}" | grep -q "$container_name"; then
+            local container_status=$(docker inspect -f "{{.State.Status}}" "$container_name")
+            if [ "$container_status" == "running" ]; then
+                echo "$container_name is ready."
+                return 0
+            fi
+        fi
+        sleep "$wait_seconds"
+    done
+
+    echo "Timed out waiting for $container_name to be ready."
+    exit 1
+}
+
+
+# Check if required dependencies are available
+command -v curl >/dev/null 2>&1 || { echo >&2 "curl is required but not installed. Aborting."; exit 1; }
+command -v openssl >/dev/null 2>&1 || { echo >&2 "openssl is required but not installed. Aborting."; exit 1; }
+command -v tar >/dev/null 2>&1 || { echo >&2 "tar is required but not installed. Aborting."; exit 1; }
+
+# Parse command line options using getopt
+OPTIONS=$(getopt -o t:p:h --long tarball:,decryption-password:,help -n 'gesund-platform-installer.sh' -- "$@")
+eval set -- "$OPTIONS"
+
+# Parse named parameters
+while true; do
+    case "$1" in
+        -t | --tarball)
+            ENCRYPTED_TARBALL_NAME="$2"
+            shift 2
+            ;;
+        -p | --decryption-password)
+            DECRYPTION_PASSWORD="$2"
+            shift 2
+            ;;
+        -h | --help)
+            show_help
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+# Check if required parameters are provided
+if [ -z "$ENCRYPTED_TARBALL_NAME" ]; then
+    echo "Error: Encrypted tarball name not provided (--tarball option)"
+    show_help
+    exit 1
+fi
+if [ -z "$DECRYPTION_PASSWORD" ]; then
+    echo "Error: Decryption password not provided (--decryption-password option)"
+    show_help
+    exit 1
+fi
+
+# Check if encrypted tarball already exists
+if [ -e "encrypted_tarball.tar.gz.enc" ]; then
+    echo "Encrypted tarball already exists. Skipping download."
+else
+    S3_URL="https://gesund-platform-releases.s3.amazonaws.com/$ENCRYPTED_TARBALL_NAME"
+    
+    HTTP_STATUS=$(curl -Is --head -w "%{http_code}" "$S3_URL" -o /dev/null)
+
+    if [ "$HTTP_STATUS" -eq 200 ]; then
+        
+        echo "Downloading encrypted tarball from $S3_URL"
+        curl -# -o encrypted_tarball.tar.gz.enc "$S3_URL" || handle_error "Failed to download encrypted tarball"
+    else
+        echo "The file may not be accessible or do not exist."
+        echo "Check the tarball name or get in touch with Gesund team."
+        handle_error "HTTP status code: $HTTP_STATUS"
+    fi
+
+fi
+
+# Check if decrypted tarball already exists
+if [ -e "decrypted_tarball.tar.gz" ]; then
+    echo "Decrypted tarball already exists. Skipping decryption."
+else
+    # Decrypt the downloaded tarball with a progress bar
+    echo "Decrypting the downloaded tarball..."
+    (openssl aes-256-cbc -pbkdf2 -d -in encrypted_tarball.tar.gz.enc -out decrypted_tarball.tar.gz -k "$DECRYPTION_PASSWORD" || handle_error "Failed to decrypt tarball") &
+    spinner $!
+fi
+
+
+if [ -d "gesund_platform" ]; then
+    echo "Tarball already unpacked."
+else
+    # Unpack the decrypted tarball with a progress bar
+    echo "Unpacking decrypted tarball..."
+    (tar -xvzf decrypted_tarball.tar.gz gesund_platform || handle_error "Failed to unpack decrypted tarball") &
+    spinner $!
+fi
+
+# Change directory to gesund_platform
+cd gesund_platform || handle_error "No directory gesund_platform"
+if [ -d "venv" ]; then
+    rm -rf venv
+fi
+
+# Create and activate Python virtual environment
+echo "Creating python virtual environment."
+python3 -m venv venv || handle_error "Could not create virtual env"
+source venv/bin/activate
+
+# Update pip and install gesund platform CLI
+echo "Updating pip packages"
+pip install -U pip || handle_error "Failed to update pip"
+GESUND_CLI_EXC=$(readlink -m venv/bin/gesund)
+GESUND_CLI_WHEEL=$(ls ./*.whl| head -1)
+echo "Installing gesund platform cli...$GESUND_CLI_WHEEL"
+pip install "$GESUND_CLI_WHEEL" --no-index --find-links wheels || handle_error "Failed to install gesund platform CLI"
+
+# Add alias to .bashrc
+echo "Successfully installed gesund cli"
+echo "Please note: added following command to .bashrc file"
+echo "alias gesund=$GESUND_CLI_EXC" >> ~/.bashrc || handle_error "Failed to add alias to .bashrc"
+
+
+
+if [ "$START_SERVICES" -eq 1 ]; then
+    LOG_FILE="gesund-installation-cli.log"
+
+    echo "Starting gesund services"
+    echo "You can view the installation logs in '$LOG_FILE' or on the screen using 'tail -f $LOG_FILE'."
+    nohup gesund start --fresh --web > "$LOG_FILE" 2>&1 &
+
+
+    echo "Waiting for Application startup to complete"
+    while ! grep -q "Application startup complete." "$LOG_FILE"; do
+        sleep 1
+    done
+
+    echo Checking if services are in a healthy state
+    containers=("mongodb" "autodeploy" "mlutility" "db_manager" "nodejs-tool" "validation" "rabbitmq" "web-server" "web-client")
+    for container in "${containers[@]}"; do
+        wait_for_container "$container"
+    done
+
+    echo "All services are ready. Installation successful."
+    exit
+fi
+
+echo "Gesund Platform installation done."
--- platform_artifacts/instructions-on-prem-installation.md
+++ platform_artifacts/instructions-on-prem-installation.md
@@ -1,74 +1,92 @@
-1. Connect to the server using SSH or any preferred method.
+# Gesund Platform Installation Guide
 
-    
-    ```
+This guide will walk you through the simplified installation process of the Gesund Platform.
+
+## Prerequisites
+
+- Access to a server with SSH or your preferred remote access method.
+- Docker installed and configured on your server.
+- The Gesund team should have provided you with the necessary decryption password and credentials to log in to the Gesund Platform.
+
+## Installation Steps
+
+1. **Connect to the Server**
+
+    Use SSH or your preferred method to connect to your server. Replace `username` and `yourserver` with your server's username and address.
+
+    ```bash
     ssh username@yourserver
     ```
-    
-2. Create a deployment directory
-    
-    ```
+
+2. **Create a Deployment Directory**
+
+    Create a deployment directory on your server:
+
+    ```bash
     mkdir deployment/ && cd deployment
     ```
-    
-3. Download the tarball from S3
-    
-    ```
-    
-    aws s3 cp s3://PATH/TO/TARBALL YOUR/LOCAL/PATH --sse AES256
-    ```
-    
-4. Download the installation script (install.sh) 
-    
-    ```
-    aws s3 cp s3://PATH/TO/INSTALLATION_SCRIPT YOUR/LOCAL/PATH
-    ```
-5. Decrypt the tarball
-    Use the password provided by Gesund team
-    ```
-    openssl  aes-256-cbc -d -in gesund_platform*.tar.enc -out gesund_platform*.tar
+
+3. **Download the Installation Script**
+
+    You can either obtain the installation script from the Gesund team directly or download it from the publicly accessible URL:
+
+    **Option 1: Gesund Team Provides the Script**
+
+    If the Gesund team provides you with the installation script directly, you can skip the download step.
+
+    **Option 2: Download from Public URL**
+
+    To download the installation script from the public URL, use the following command:
+
+    ```bash
+    curl -o gesund-platform-installer.sh https://gesund-platform-releases.s3.amazonaws.com/gesund-platform-installer.sh
     ```
 
-6. Execute the `install.sh` script with the appropriate tarball path.
-    
+4. **Execute the Installation Script**
+
+    Execute the `gesund-platform-installer.sh` script. This command will automatically:
+
+    - Download the encrypted tarball.
+    - Decrypt the tarball using the provided password.
+    - Install the Gesund Platform CLI.
+
+    ```bash
+    ./gesund-platform-installer.sh --tarball gesund_platform*.tar.enc --decryption-password PASSWORD 
     ```
-    TARBALL_PATH="/path/to/tarball/" ./install.sh
+    Replace `gesund_platform*.tar.enc` with the actual tarball filename and `PASSWORD` with the provided decryption password.
+
+    Run the `gesund-platform-installer.sh` script and wait for the installation to finish. 
+
+
+    ###  Command-line Options
+
+    The script supports the following command-line options:
+
+    - `-t, --tarball NAME`: Specifies the name of the encrypted tarball on S3 (required).
+    - `-d, --decryption-password PASSWORD`: Specifies the decryption password (required).
+    - `-h, --help`: Shows the help message and exits.
+
     
+    
+
+5. **Access the Platform**
+
+    Open a web browser and access the Gesund Platform using the public IP address of your server. Replace `SERVER_PUBLIC_IP` with the actual public IP address.
+
     ```
-    
-    This command will install the platform CLI.
-    
-7. Optionally, you can start the services directly by setting the `START_SERVICES` environment variable and skipping step 4.
-    
+    http://SERVER_PUBLIC_IP
     ```
-    START_SERVICES=1 TARBALL_PATH="/path/to/tarball/" ./install.sh
-    
-    ```
-    
-8. Execute the `gesund-cli` command.
-    
-    ```
-    gesund start --fresh --web
-    
-    ```
-    
-9. Open a web browser and access the platform using the public IP address. (e.g)
-    
-    ```
-    <http://SERVER_PUBLIC_IP>
-    
-    ```
-    
-    Replace `SERVER_PUBLIC_IP` with the actual public IP address of the server.
-    
-10. Check if the services are running. Run the following command:
-    
-    ```
+
+6. **Check Running Services**
+
+    To check if the services are running, run the following command:
+
+    ```bash
     docker ps
     ```
-    
-    You should be able the following service container running:
-    
+
+    You should see the following service containers running:
+
     - mlutility
     - gesund-docs
     - web-client
@@ -78,17 +96,17 @@
     - nodejs-tool
     - mongodb
     - rabbitmq
-11. Enter the provided credentials to log in to the platform.
-    - Username: [admin@gesund.ai](mailto:admin@gesund.ai)
-    - Password:
-    
-    The password can be obtained by getting the instance ID e.g
-    
-    ```
-    sudo dmidecode | grep UUID
-    > UUID: 123a5b-293c-41ec-aa4e-9ff04f99f3e7
-    ```
-12. Listed below are the container sizes for various components of the "gesund" services:
+
+7. **Log In to the Platform**
+
+    Enter the provided credentials to log in to the Gesund Platform:
+
+    - Username: admin@gesund.ai
+    - Password: [Contact the Gesund team for the password]
+
+8. **Container Sizes**
+
+    Here are the container sizes for various components of the Gesund Platform:
 
     ```
     gesundai/gesund-app            259MB
